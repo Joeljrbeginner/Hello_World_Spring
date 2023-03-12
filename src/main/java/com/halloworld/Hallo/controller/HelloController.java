426608cafdcb03a556f21eac087b4f1f4d9fd8b7
Joeljrbeginner <joeljuniormooca364@gmail.com>
2023-03-12 17:19:50 -0300

    modified camada controller

--- src/main/java/com/halloworld/Hallo/controller/HelloController.java
+++ src/main/java/com/halloworld/Hallo/controller/HelloController.java
@@ -1,14 +1,28 @@
 package com.halloworld.Hallo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
+
+    @Autowired
+    private HelloService helloService;
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello World!";
+        return helloService.getHelloMessage();
     }
 }
 
+@Service
+public class HelloService {
+
+    public String getHelloMessage() {
+        return "Hello World!";
+    }
+}
